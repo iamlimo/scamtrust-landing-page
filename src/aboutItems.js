f8003d143b2de8704eb923aff66acd8955ffea11
michael <you@example.com>
2022-12-14 22:36:52 +0100

    wednessday commit

--- src/aboutItems.js
+++ src/aboutItems.js
@@ -1,55 +1,59 @@
 import img1 from '../src/Assets/img-1.png'
+import Fade from '../src/Assets/Fade.jpg'
+import Fisayo from '../src/Assets/Fisayo.jpg'
+import Dami from '../src/Assets/Dami.jpg'
+import Ara from '../src/Assets/Ara.jpg'
+import Michael from '../src/Assets/Michael.jpg'
+import Peace from '../src/Assets/Peace.jpeg'
 import img5 from '../src/Assets/img-5.png'
-import img6 from '../src/Assets/img-6.png'
-import img8 from '../src/Assets/orange-img.png'
 
 const aboutItems = [
     {
         id: 1,
         img: img1,
-        name: "Bankulee",
+        name: "Kingsley Kennedy",
         position: `C E O`,
     },    
     {
         id: 2,
         img: img5,
-        name: "Issac Orija",
+        name: "Isaac Orija",
         position: "C T O",
     },    
     {
         id: 3,
-        img: img6,
+        img: Peace,
         name: "Peace Okoye",
         position: "Product manager",
     },    
     {
         id: 4,
-        img: img8,
-        name: "Fade",
+        img: Fade,
+        name: "Fadekemi Adefolalu",
         position: "Product marketing manager",
     },    
     {
         id: 5,
-        img: img8,
+        img: Michael,
         name: "Michael Olaniyan",
         position: "Frontend Developer",
     }, 
     {
         id: 6,
-        img: img6,
+        img: Fisayo,
         name: "Oke Fisayo",
         position: "Designer",
     },       
     {
         id: 7,
-        img: img1,
-        name: "Dami",
+        img: Dami,
+        name: "Damilola Oladepo",
         position: "Customer support",
     },    
     {
         id: 8,
-        img: img5,
-        name: "Ara",
+        img: Ara,
+        name: "Adeoti Araoluwa",
         position: "Customer support",
     },    
 ]
