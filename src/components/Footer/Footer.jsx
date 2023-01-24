2db2fb6fbacd97506d9b9289275ecb5ea73941e2
Olaniyan michael <olaniyanmichael0@gmail.com>
2023-01-24 13:09:40 +0100

    newsletter added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Footer.css'
 import {HiArrowRight} from 'react-icons/hi'
 import { Link } from 'react-router-dom'
@@ -10,19 +10,82 @@ import {TbMinusVertical} from 'react-icons/tb'
 
 
 const Footer = () => {
+  const [email, setEmail] = useState("")
+  const [option, setOption] = useState("")
+  const [radio, setRadio] = useState("")
+
+  const handleChange = (e) => {
+    setEmail(e.target.value);
+}
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    if (email) {
+        fetch(`/api/memberAdd?email=${email}`)
+        .then(res => res.json())
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+    }
+  
+}
+
+const radioChange = (event) => {
+  setRadio(event.currentTarget);
+}
 
   const year = new Date().getFullYear()
 
   return (
    <div className='foo-wrapper'>
     <div className='foo-con'>
+
         <div className="foo-news">
           <p className='news-title'>Subsribe to our newsletter</p>
-          <div className="input-div">
-          <input className='news-input' type="text" placeholder='Email Address' />
-          <button className='news-btn'><HiArrowRight /></button>
+          <form onSubmit={handleSubmit} className="input-div">
+          <div className="foo-form-top">
+          <input 
+          className='news-input' 
+          onChange={handleChange}
+          type="email" 
+          name='email'
+          value={email} 
+          placeholder='Email Address' 
+          />
+          <button type="submit" className='news-btn'><HiArrowRight /></button>
           </div>
+
+            {/* R A D I O */}
+          <div className='foo-radio'>
+            <div className='foo-radio-item'>
+            <div onClick={() => {setOption("Vendor")}}>
+            <input
+             type="radio"
+             name='option'
+            //  value="vendor"
+             value={radio}
+             onChange={radioChange}
+             checked={option === "Vendor"}
+              /> Vendor
+           </div>
+           </div>
+
+             <div className='foo-radio-item'>
+            <div onClick={() => {setOption("Customer")}}>
+             <input
+               className='foo-radio-item'
+              type="radio"
+              name='option'
+              value={radio}
+              onChange={radioChange}
+              checked={option === "Customer"}
+               /> Customer
+            </div>
+            </div>
+          </div>
+          </form>
         </div>
+
+
         <div className="foo-links">
           <p className='links-title'>Company</p>
           <Link className='links-li' to="/about">About</Link>
