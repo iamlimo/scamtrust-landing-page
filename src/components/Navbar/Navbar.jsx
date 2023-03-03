87619a64a4afac9bdd291bdd1e0b45e3ae73ebd3
Olaniyan michael <olaniyanmichael0@gmail.com>
2023-03-03 15:00:42 +0100

    Login link added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,8 +12,16 @@ const Navbar = () => {
        </div>
        <div className='nav-right'>
            <button className='log-btn ab-btn'><Link className='btn log-btn' to="/about">About Us</Link></button>
-           <button className='btn reg-btn log-btn-1'>Login</button>
-           <button className='sign-btn'>Sign up</button>
+           <button className='btn reg-btn log-btn-1'>
+            <a className='btn-link'
+            href="https://scamtrust-webapp.vercel.app/" target="_blank" rel="noreferrer">Login
+            </a>
+            </button>
+           <button className='sign-btn'>
+           <a className='btn-link'
+            href="https://scamtrust-webapp.vercel.app/" target="_blank" rel="noreferrer">Sign up
+            </a>
+           </button>
        </div>
     </div>
   )
